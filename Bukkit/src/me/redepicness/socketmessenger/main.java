3f5c996720b2b3f5ed013f57cdcaecebaee486d9
Red_Epicness <redepicness1@gmail.com>
2015-11-13 20:35:21 +0100

    Setting up main classes.

---
+++ Bukkit/src/me/redepicness/socketmessenger/main.java
@@ -0,0 +1,11 @@
+package me.redepicness.socketmessenger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+
+    }
+}
---
+++ Bungee/src/me/redepicness/socketmanager/main.java
@@ -0,0 +1,11 @@
+package me.redepicness.socketmanager;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class main extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
